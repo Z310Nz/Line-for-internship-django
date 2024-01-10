914490516af2c03a8fca52d6f2aee0420a352cd4
Pratya Palee <pratya.palee@gmail.com>
2024-01-10 16:32:48 +0700

    company regis done

--- company/urls.py
+++ company/urls.py
@@ -1,12 +1,11 @@
 # company/urls.py
 from django.urls import path
 from .views import login
-from .views import register
+from .views import register_company
 
 app_name = 'company'  # Add this line
 
 urlpatterns = [
     path('logincom/', login, name='login'),
-    # Add other URLs as needed
-    path('registercom/', register, name="register"),
+    path('registercom/', register_company, name='register_company'),
 ]
--- company/views.py
+++ company/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CompanyForm, JobForm
 
 # Create your views here.
 def login(request):
     # Your login view logic goes here
     return render(request, 'logincom.html')
 
-def register(request):
-    # Your register view logic goes here
-    return render(request, 'registercom.html')
+def register_company(request):
+    if request.method == 'POST':
+        company_form = CompanyForm(request.POST)
+        job_form = JobForm(request.POST)  # สร้าง JobForm ขึ้นมา (ต้องให้ได้ JobForm ในที่นี้)
+        if company_form.is_valid() and job_form.is_valid():
+            company = company_form.save()
+            job = job_form.save(commit=False)
+            job.company = company  # กำหนดบริษัทให้กับ Job
+            job.save()
+            return redirect('success_page')
+    else:
+        company_form = CompanyForm()
+        job_form = JobForm()  # สร้าง JobForm ขึ้นมา (ต้องให้ได้ JobForm ในที่นี้)
+
+    return render(request, 'registercom.html', {'company_form': company_form, 'job_form': job_form})
 
 def profile(request):
     # Your profile view logic goes here
