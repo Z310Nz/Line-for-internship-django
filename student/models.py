0d882a2a4d8d4a00d1f39b91fc5e9138e9932dbd
Pratya Palee <pratya.palee@gmail.com>
2024-01-20 15:59:31 +0700

    reset project

--- student/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Student(models.Model):
-    # Your model fields go here
-    name = models.CharField(max_length=100)
-    # Add other fields as needed
