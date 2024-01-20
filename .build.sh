b4982a779c1a58b183d8bc2ecfaa2f54be8142e9
Pratya Palee <pratya.palee@gmail.com>
2024-01-20 14:36:05 +0700

    .

--- .build.sh
+++ .build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+source venv/bin/activate
 
 # Install Python dependencies from requirements.txt
 pip install -r requirements.txt
 
 # Install additional packages like line-bot-sdk and django-allauth
+pip install django
 pip install line-bot-sdk
 pip install django-allauth
 
